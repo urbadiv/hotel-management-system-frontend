5864d6b2d3b380354dd04dce3e3b3ba2dc8282f2
dinethrashmikaheshan <dineth550@gmail.com>
2024-12-04 15:34:58 +0530

    event

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -6,7 +6,7 @@ const EventCard = ({ event }) => {
             <h3 className="text-xl font-bold">{event.name}</h3>
             <p>Date: {new Date(event.date).toLocaleDateString()}</p>
             {event.banner && (
-                <img src={event.banner} alt={event.name} className="mt-2 w-full h-48 object-cover rounded" />
+                <img src={`http://localhost:8070/uploads/${event.banner}`} alt={event.name} className="mt-2 w-full h-48 object-cover rounded" />
             )}
         </div>
     );
